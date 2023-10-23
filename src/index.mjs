9456ba2191b9c40048e3e19765fb1a99d6442318
haryomogi <148606350+haryomogi@users.noreply.github.com>
2023-10-23 10:04:41 +0900

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,24 @@
+/**
+ * const, let等の変数宣言
+ */
+// var val1 ="var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "update var";
+// console.log(val1);
+
+// letは上書き可能だが、再宣言できない
+// let val2 ="let variable";
+// console.log(val2);
+
+// テンプレート文字列``で囲う
+const name = "hoge";
+const age = "28";
+const message = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message);
+
+
+
+
+
